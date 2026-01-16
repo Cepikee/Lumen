93713bb20899ceedaa846354d541ae88df5d2957
Yosohara <vashiri6562@gmail.com>
2026-01-16 21:40:57 +0100

    Pipeline cron javítás

--- app/api/hello/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import "../../../lib/cron"; // 🔥 cron modul betöltése
-
-export async function GET() {
-  return NextResponse.json({ message: "Hello World + Cron fut!" });
-}
--- cron.ts
+++
@@ -1,9 +0,0 @@
-import cron from "node-cron";
-
-// 5 percenként fut
-cron.schedule("*/5 * * * *", async () => {
-  console.log("Automatikus összefoglalás indul...");
-
-  // Meghívja a summarize-all API-t
-  await fetch("http://localhost:3000/api/summarize-all");
-});
