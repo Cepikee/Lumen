6213edf79e2a8a5dec632f800be4bca362d27592
Yosohara <vashiri6562@gmail.com>
2026-01-24 12:36:14 +0100

    automatikus videó

--- app/hirado/page.tsx
+++ app/hirado/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from "next/dynamic";
 import HiradoArchive from "@/components/HiradoArchive";
 const HiradoPlayer = dynamic(() => import("@/components/HiradoPlayer"), {
