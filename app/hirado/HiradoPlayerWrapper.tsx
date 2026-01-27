ec4b41f6af38499ea0d89c402c862d1739b6a52b
Yosohara <vashiri6562@gmail.com>
2026-01-27 11:25:28 +0100

    Signed url

--- app/hirado/HiradoPlayerWrapper.tsx
+++ app/hirado/HiradoPlayerWrapper.tsx
@@ -3,21 +3,11 @@
 import dynamic from "next/dynamic";
 import type { HiradoPlayerProps } from "@/components/HiradoPlayer";
 
-const HiradoPlayer = dynamic<HiradoPlayerProps>(
+const HiradoPlayer = dynamic(
   () => import("@/components/HiradoPlayer"),
   { ssr: false }
 );
 
-export default function HiradoPlayerWrapper({
-  video,
-  isPremium,
-  videoUrl,
-}: HiradoPlayerProps) {
-  return (
-    <HiradoPlayer
-      video={video}
-      isPremium={isPremium}
-      videoUrl={videoUrl}
-    />
-  );
+export default function HiradoPlayerWrapper(props: HiradoPlayerProps) {
+  return <HiradoPlayer {...props} />;
 }
