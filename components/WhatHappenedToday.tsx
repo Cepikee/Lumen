398a1084ba78cd27abc21be54137657d9a72f92f
Yosohara <vashiri6562@gmail.com>
2026-02-22 14:23:12 +0100

    02.21: WhatHappenedToday továb fejlesztése, ez a stabil verzió.

--- components/WhatHappenedToday.tsx
+++ components/WhatHappenedToday.tsx
@@ -12,41 +12,47 @@ export default function WhatHappenedToday() {
       {/* KÖZÖS DOBOZ */}
       <div className="wht-wrapper-box p-4">
 
-        {/* DOBOZ FEJLÉC */}
-        <div className="wht-header text-center mb-4">
+        {/* CÍM */}
+        <div className="wht-header text-center mb-5">
           <h2 className="wht-title d-inline-block px-3">
             Mi történt ma valójában?
           </h2>
         </div>
 
-        {/* 4 MODUL GRID */}
-        <div className="row g-4">
+        {/* 1. SOR: KATEGÓRIÁK + KIUGRÓ AKTIVITÁSOK */}
+        <div className="row g-4 mb-4">
 
-          <div className="col-12 col-md-6 col-lg-3">
+          <div className="col-12 col-lg-6">
             <div className="wht-box module-box">
               <WhatHappenedTodayHeatmap />
             </div>
           </div>
 
-          <div className="col-12 col-md-6 col-lg-3">
-            <div className="wht-box module-box">
-              <WhatHappenedTodaySourceActivity />
-            </div>
-          </div>
-
-          <div className="col-12 col-md-6 col-lg-3">
+          <div className="col-12 col-lg-6">
             <div className="wht-box module-box">
               <WhatHappenedTodaySpikeDetection />
             </div>
           </div>
 
-          <div className="col-12 col-md-6 col-lg-3">
+        </div>
+
+        {/* 2. SOR: FORRÁSOK + KULCSSZAVAK */}
+        <div className="row g-4">
+
+          <div className="col-12 col-lg-6">
+            <div className="wht-box module-box">
+              <WhatHappenedTodaySourceActivity />
+            </div>
+          </div>
+
+          <div className="col-12 col-lg-6">
             <div className="wht-box module-box">
               <WhatHappenedTodayKulcsszavak />
             </div>
           </div>
 
         </div>
+
       </div>
     </div>
   );
