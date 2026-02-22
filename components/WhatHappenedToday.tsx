fb6bd29e9eb01a6ba92d1a9d390467a8b1d372ee
Yosohara <vashiri6562@gmail.com>
2026-02-22 14:42:21 +0100

    02.21: WhatHappenedToday továb fejlesztése, ez a stabil verzió.

--- components/WhatHappenedToday.tsx
+++ components/WhatHappenedToday.tsx
@@ -7,48 +7,43 @@ import WhatHappenedTodayKulcsszavak from "./WhatHappenedTodayKulcsszavak";
 
 export default function WhatHappenedToday() {
   return (
-    <div className="container my-5 d-flex justify-content-center">
+    <div className="wht-wrapper-box">
 
-      {/* NAGY DOBOZ */}
-      <div className="wht-wrapper-box p-4">
+      {/* CÍM */}
+      <div className="wht-header text-center mb-5">
+        <h2 className="wht-title d-inline-block px-3">
+          Mi történt ma valójában?
+        </h2>
+      </div>
 
-        {/* CÍM */}
-        <div className="wht-header text-center mb-5">
-          <h2 className="wht-title d-inline-block px-3">
-            Mi történt ma valójában?
-          </h2>
+      {/* FŐSOR – 3 OSZLOP */}
+      <div className="row g-4">
+
+        {/* BAL OSZLOP */}
+        <div className="col-12 col-lg-4">
+          <div className="wht-box module-box">
+            <WhatHappenedTodayHeatmap />
+          </div>
         </div>
 
-        {/* EGY SOR – 3 BLOKK EGYMÁS MELLETT */}
-        <div className="row g-4">
+        {/* KÖZÉPSŐ OSZLOP */}
+        <div className="col-12 col-lg-4">
+          <div className="wht-box module-box">
+            <WhatHappenedTodaySourceActivity />
+          </div>
+        </div>
 
-          {/* BAL BLOKK – KATEGÓRIÁK */}
-          <div className="col-12 col-lg-4">
-            <div className="wht-box module-box">
-              <WhatHappenedTodayHeatmap />
-            </div>
+        {/* JOBB OSZLOP */}
+        <div className="col-12 col-lg-4 d-flex flex-column gap-4">
+
+          {/* FENT – KIUGRÓ AKTIVITÁSOK (VÍZSZINTES) */}
+          <div className="wht-box module-box horizontal-list">
+            <WhatHappenedTodaySpikeDetection />
           </div>
 
-          {/* KÖZÉPSŐ BLOKK – FORRÁSOK */}
-          <div className="col-12 col-lg-4">
-            <div className="wht-box module-box">
-              <WhatHappenedTodaySourceActivity />
-            </div>
-          </div>
-
-          {/* JOBB BLOKK – KÉT ELEM EGY OSZLOPBAN (A JOBB OLDALON A TARTALOM VÍZSZINTESEN JELENIK MEG) */}
-          <div className="col-12 col-lg-4 d-flex flex-column gap-4">
-
-            {/* FENT – KIUGRÓ AKTIVITÁSOK (VÍZSZINTES LISTA) */}
-            <div className="wht-box module-box horizontal-box">
-              <WhatHappenedTodaySpikeDetection />
-            </div>
-
-            {/* LENT – FELKAPOTT KULCSSZAVAK (VÍZSZINTES LISTA) */}
-            <div className="wht-box module-box horizontal-box">
-              <WhatHappenedTodayKulcsszavak />
-            </div>
-
+          {/* LENT – KULCSSZAVAK (VÍZSZINTES) */}
+          <div className="wht-box module-box horizontal-list">
+            <WhatHappenedTodayKulcsszavak />
           </div>
 
         </div>
