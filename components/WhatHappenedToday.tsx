8a0b64034b163ef9284f7635d3e546e5ea55f0ac
Yosohara <vashiri6562@gmail.com>
2026-02-24 10:40:51 +0100

    02.24: Tailwind bevezetése, WhatHappenedTodayKulcsszavak fejlesztése v1

--- components/WhatHappenedToday.tsx
+++ components/WhatHappenedToday.tsx
@@ -7,8 +7,8 @@ import WhatHappenedTodayKulcsszavak from "./WhatHappenedTodayKulcsszavak";
 
 export default function WhatHappenedToday() {
   return (
-    <div className="wht-wrapper-box">
-
+    // A wrapper most adjon elegendő magasságot az oldalnak (header magasságától függően finomítsd)
+    <div className="wht-wrapper-box min-h-[calc(100vh-120px)]">
       {/* CÍM */}
       <div className="wht-header text-center mb-5">
         <h2 className="wht-title d-inline-block px-3">
@@ -16,38 +16,43 @@ export default function WhatHappenedToday() {
         </h2>
       </div>
 
-      {/* FŐSOR – 3 OSZLOP */}
-      <div className="row g-4">
+      {/* FŐSOR – 3 OSZLOP; stretch-eljük a sor magasságát */}
+      <div className="row g-4 items-stretch h-full">
 
-        {/* BAL OSZLOP – kisebb: 3 */}
-        <div className="col-12 col-lg-3">
-          <div className="wht-box module-box">
+        {/* BAL OSZLOP – 3 */}
+        <div className="col-12 col-lg-3 d-flex flex-column gap-4 h-full">
+          <div className="wht-box module-box p-0">
             <WhatHappenedTodayHeatmap />
           </div>
-        </div>
 
-        {/* KÖZÉPSŐ OSZLOP – kisebb: 3 */}
-        <div className="col-12 col-lg-3">
-          <div className="wht-box module-box">
+          <div className="wht-box module-box p-0">
             <WhatHappenedTodaySourceActivity />
           </div>
         </div>
 
-        {/* JOBB OSZLOP – nagyobb: 6 */}
-        <div className="col-12 col-lg-6 d-flex flex-column gap-4">
-
-          {/* FENT – KIUGRÓ AKTIVITÁSOK (VÍZSZINTES) */}
-          <div className="wht-box module-box horizontal-list">
+        {/* KÖZÉPSŐ OSZLOP – 3 */}
+        <div className="col-12 col-lg-3 d-flex flex-column gap-4 h-full">
+          <div className="wht-box module-box p-0">
             <WhatHappenedTodaySpikeDetection />
           </div>
+        </div>
 
-          {/* LENT – KULCSSZAVAK (VÍZSZINTES) */}
-          <div className="wht-box module-box horizontal-list p-0 overflow-hidden">
-            <div className="px-3 py-2">
-            <WhatHappenedTodayKulcsszavak />
+        {/* JOBB OSZLOP – 6: itt a fontos rész */}
+        <div className="col-12 col-lg-6 d-flex flex-column gap-4 h-full">
+          {/* FENT – KIUGRÓ AKTIVITÁSOK (fix magasságú doboz) */}
+          <div className="wht-box module-box p-0 flex-none">
+            <div className="p-3">
+              <WhatHappenedTodaySpikeDetection />
             </div>
           </div>
 
+          {/* LENT – KULCSSZAVAK (EZ LESZ A GÖRGETHETŐ, KITÖLTŐ DOBOZ) */}
+          <div className="wht-box module-box p-0 flex-1 overflow-hidden">
+            {/* belső wrapper, ami görgethető */}
+            <div className="h-full overflow-y-auto p-3">
+              <WhatHappenedTodayKulcsszavak />
+            </div>
+          </div>
         </div>
 
       </div>
