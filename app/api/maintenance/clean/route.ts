b6519e7c89a7b6a5ff83d8e4a56c1b13f82fa245
Yosohara <vashiri6562@gmail.com>
2026-03-04 21:58:08 +0100

    api: HEatmap javítva JAVíTOTT GECISZOPÓ

--- app/api/maintenance/clean/route.ts
+++ app/api/maintenance/clean/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { exec } from "child_process";
 
 export async function GET() {
-  return new Promise((resolve) => {
+  return new Promise<NextResponse>((resolve) => {
     exec("node scripts/cleanCategoriesRunner.js", (err, stdout, stderr) => {
       if (err) {
         console.error("CLEAN ERROR:", err);
